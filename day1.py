c0d70e2b98876111d9e5e1b036e270850a82cf19
ying12345 <guyingw@yahoo.com>
2018-12-03 10:16:46 -0600

    Update day1.py

--- day1.py
+++ day1.py
@@ -13,3 +13,10 @@ for i in range (0, len(lines)):
 
 print("finalFrequency is:", currentFrequency )
   
+currentFrequency=0
+frequencies=[]
+for line in lines:
+        currentFrequency+=int(line)
+        if currentFrequency in frequencies:
+                print("The first frequency reached twice is:", currentFrequency)
+        frequencies.append(currentFrequency)
